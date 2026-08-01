fe094bd6df8e46a6e2276ceeab593aedf32389c0
KATHIRAVAN-R <iamkathiravan1@gmail.com>
2026-08-01 22:06:12 +0530

    changes in js

--- js/index.js
+++ js/index.js
@@ -82,6 +82,7 @@ function calc() {
   // res_cont.style.color = "#669CFF";
   // res_cont.style.textAlign = "center";
 // res_cont.style.background = "inear-gradient(151deg, rgba(2, 0, 36, 0.5) 0%, rgba(9, 9, 121, 0.5) 17%,rgba(0, 212, 255, 0.5) 100% ";
+  document.getElementById("res-name").innerHTML = "Name : " + name;
   document.getElementById("res-total").innerHTML = "Total : " + sum;
   document.getElementById("res-avg").innerHTML = "Average : " + avg;
   document.getElementById("res-grade").innerHTML = "Grade : " + grade;
